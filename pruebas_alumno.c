0782824c1e5065b1bdda961c7f036e370494db64
andreseliseo <andres.soncini@gmail.com>
2017-05-29 23:33:02 -0300

    Prueba con heap_desencolar

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -38,11 +38,11 @@ void prueba_heap_insersion(void) {
 	void* maximo;
 	char* str1 = malloc(sizeof(char) + 1);
 	char* str2 = malloc(sizeof(char) + 1);
-	//char* str3 = malloc(sizeof(char) + 1);
+	char* str3 = malloc(sizeof(char) + 1);
 	
 	strcpy(str1, "a");
 	strcpy(str2, "b");
-	//strcpy(str3, "c");
+	strcpy(str3, "c");
 
 	print_test("Prueba heap la cantidad de elementos es 0", heap_cantidad(heap) == 0);
 	print_test("Prueba insertar 'a' inserta correctamente", heap_encolar(heap, str1));
@@ -54,13 +54,37 @@ void prueba_heap_insersion(void) {
 	print_test("Prueba heap la cantidad de elementos es 2", heap_cantidad(heap) == 2);
 	maximo = heap_ver_max(heap);
 	print_test("Prueba heap maximo es 'b'", (strcmpaux(maximo,"b") == 0));
+	print_test("Prueba heap la cantidad de elementos es 2", heap_cantidad(heap) == 2);
+	maximo = heap_ver_max(heap);
+	print_test("Prueba heap maximo es 'b' (no se modifica heap al ver maximo)", (strcmpaux(maximo,"b") == 0));
+	print_test("Prueba heap la cantidad de elementos es 2", heap_cantidad(heap) == 2);
+	maximo = heap_ver_max(heap);
+	print_test("Prueba heap maximo es 'b' (no se modifica heap al ver maximo)", (strcmpaux(maximo,"b") == 0));
+	print_test("Prueba heap la cantidad de elementos es 2", heap_cantidad(heap) == 2);
 
+	maximo = heap_desencolar(heap);
+	print_test("Prueba heap desencolar maximo es 'b'", (strcmpaux(maximo,"b") == 0));
+	print_test("Prueba heap la cantidad de elementos es 1", heap_cantidad(heap) == 1);
+	maximo = heap_desencolar(heap);
+	print_test("Prueba heap desencolar maximo es 'a'", (strcmpaux(maximo,"a") == 0));
+	print_test("Prueba heap la cantidad de elementos es 0", heap_cantidad(heap) == 0);
 
+	print_test("Prueba insertar 'b' inserta correctamente", heap_encolar(heap, str2));
+	print_test("Prueba insertar 'a' inserta correctamente", heap_encolar(heap, str1));
+	print_test("Prueba insertar 'c' inserta correctamente", heap_encolar(heap, str3));
+	print_test("Prueba heap la cantidad de elementos es 3", heap_cantidad(heap) == 3);
+
+	maximo = heap_desencolar(heap);
+	print_test("Prueba heap desencolar maximo es 'c'", (strcmpaux(maximo,"c") == 0));
+	print_test("Prueba heap la cantidad de elementos es 2", heap_cantidad(heap) == 2);
+	maximo = heap_desencolar(heap);
+	print_test("Prueba heap desencolar maximo es 'b'", (strcmpaux(maximo,"b") == 0));
+	print_test("Prueba heap la cantidad de elementos es 1", heap_cantidad(heap) == 1);
+	maximo = heap_desencolar(heap);
+	print_test("Prueba heap desencolar maximo es 'a'", (strcmpaux(maximo,"a") == 0));
+	print_test("Prueba heap la cantidad de elementos es 0", heap_cantidad(heap) == 0);
 
 	heap_destruir(heap, free);
-	//heap_destruir(heap, NULL);
-	//free(str1);
-	//free(str2);
 }
 
 void pruebas_heap_alumno(void) {
