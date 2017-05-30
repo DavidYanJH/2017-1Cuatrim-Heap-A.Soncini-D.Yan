7207a97dbcb246862de6a1e84c01c66d2bc41313
DavidYanJH <DavidYanJH@users.noreply.github.com>
2017-05-29 23:53:57 -0300

    Add files via upload

--- heap.c
+++ heap.c
@@ -6,7 +6,7 @@
 #define COEF_REDIM 2
 
 struct heap {
-	void** vec_dinam;
+	void** datos;
 	cmp_func_t cmp;
 	size_t cantidad;
 	size_t capacidad;
@@ -27,19 +27,57 @@ size_t obtener_hijo_derecho(size_t index) {	// Determinar si van a ser de utilid
 	return index * 2 + 2;
 }
 
-void swap(void** vector, size_t index_hijo, void* dato_hijo, size_t index_padre, void* dato_padre) {
-	vector[index_hijo] = dato_padre;
-	vector[index_padre] = dato_hijo;
+void swap_vectorial(void** vector, size_t x,  size_t y) {
+	void* aux = vector[x];
+	vector[x] = vector[y];
+	vector[y] = aux;
 }
 
-void upheap(heap_t* heap, size_t index_hijo, void* dato_hijo) {
-	if (index_hijo == 0) return;
-	size_t index_padre = obtener_padre(index_hijo);
-	void* dato_padre = heap->vec_dinam[index_padre];
-	if (heap->cmp(dato_hijo, dato_padre) > 0) {
-		swap(heap->vec_dinam, index_hijo, dato_hijo, index_padre, dato_padre);
-		upheap(heap, index_padre, dato_hijo);
+void upheap(heap_t* heap, size_t actual) {
+	if (actual == 0) return;
+	size_t padre = obtener_padre(actual);
+	if (heap->cmp(heap->datos[actual], heap->datos[padre]) > 0) {
+		swap_vectorial(heap->datos, actual, padre);
+		upheap(heap, padre);
 	}
+}
+
+
+void downheap(heap_t* heap, size_t actual)
+{
+	if (actual == heap->cantidad) return;
+	size_t hijo_izq = obtener_hijo_izquierdo(actual);
+	size_t hijo_der = obtener_hijo_derecho(actual);
+	// Caso sin hijo izquierdo valido es una condición de finalizar la recursión
+	if (hijo_izq > heap->cantidad) return;
+	// Caso sin hijo derecho valido, swappeo según necesidad con hijo izquierdo
+	// luego finalizo la recursión
+	if (hijo_der > heap->cantidad) {
+		if (heap->cmp(heap->datos[hijo_izq], heap->datos[actual]) > 0) {
+			swap_vectorial(heap->datos, hijo_izq, actual);
+			return;
+		}
+	}
+	// Caso con hijos izquierdo e hijo derecho, swappeo con el mayor de los hijos 
+	// según necesidad
+	if (heap->cmp(heap->datos[hijo_der], heap->datos[hijo_izq]) > 0) {
+		swap_vectorial(heap->datos, hijo_der, actual);
+		downheap(heap, hijo_der);
+	}
+	else {
+		swap_vectorial(heap->datos, hijo_izq, actual);
+		downheap(heap, hijo_izq);
+	}
+}
+
+bool heap_redimensionar(heap_t* heap) 
+{
+	size_t new_capacidad = heap->capacidad * COEF_REDIM;
+	void** new_datos = realloc(heap->datos, sizeof(void*) * new_capacidad);
+	if (!new_datos) return false;
+	heap->datos = new_datos;
+	heap->capacidad = new_capacidad;
+	return true;
 }
 
 /* ******************************************************************
@@ -51,8 +89,8 @@ heap_t* heap_crear(cmp_func_t cmp)
 	if (!cmp) return NULL;
 	heap_t* heap = malloc(sizeof(heap_t));
 	if (!heap) return NULL;
-	heap->vec_dinam = malloc(sizeof(void*) * CAPAC_INICIAL);
-	if (!heap->vec_dinam) {
+	heap->datos = malloc(sizeof(void*) * CAPAC_INICIAL);
+	if (!heap->datos) {
 		free(heap);
 		return NULL;
 	}
@@ -80,36 +118,41 @@ bool heap_esta_vacio(const heap_t* heap)
 void* heap_ver_max(const heap_t* heap)
 {
 	if (heap_esta_vacio(heap)) return NULL;
-	return heap->vec_dinam[0];
+	return heap->datos[0];
 }
 
 
 void heap_destruir(heap_t* heap, void destruir_elem(void* e))
 {
 	if (!heap_esta_vacio(heap) && destruir_elem) {
-		for (size_t index = 0; index < heap->cantidad; ++index) {
-			void* tmp = heap->vec_dinam[index];
-			destruir_elem(tmp);
-		}
+		for (size_t index = 0; index < heap->cantidad; ++index) 
+			destruir_elem(heap->datos[index]);
 	}
 	if (heap) {
-		free(heap->vec_dinam);
+		free(heap->datos);
 		free(heap);
 	}
 }
 
-bool heap_encolar(heap_t *heap, void *elem) {
-	if (!elem) return false;
+
+bool heap_encolar(heap_t* heap, void* elem) {
+	if (!heap || !elem) return false;
 	if (heap->cantidad == heap->capacidad) {
-		void** new_vec_dinam = realloc(heap->vec_dinam, sizeof(void*) * heap->capacidad * COEF_REDIM);
-		if (!new_vec_dinam) return false;
-		else {
-			heap->vec_dinam = new_vec_dinam;
-			heap->capacidad = heap->capacidad * COEF_REDIM;
-		}
+		if (!heap_redimensionar(heap)) 
+			return false;
 	}
-	heap->vec_dinam[heap->cantidad] = elem;
-	upheap(heap, heap->cantidad, elem);
-	heap->cantidad+=1;
+	heap->datos[heap->cantidad] = elem;
+	upheap(heap, heap->cantidad);
+	++heap->cantidad;
 	return true;
+}
+
+void* heap_desencolar(heap_t* heap)
+{
+	if (heap_esta_vacio(heap)) return NULL;
+	--heap->cantidad;
+	if (heap->cantidad == 0) return heap->datos[0];
+	swap_vectorial(heap->datos, 0, heap->cantidad);
+	downheap(heap, 0);
+	return heap->datos[heap->cantidad];
 }
